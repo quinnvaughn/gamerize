774d39f17f5c08c7e18e4ed24d1f00fa4386823c
Quinn Vaughn <quinn@gamerize.io>
2019-03-02 15:50:13 -0600

    Added highlighting to system option and removed focus from buttons.

--- frontend/src/Components/ScrollToTop.jsx
+++ frontend/src/Components/ScrollToTop.jsx
@@ -3,10 +3,7 @@ import { withRouter } from 'react-router-dom'
 
 class ScrollToTop extends Component {
   componentDidUpdate(prevProps) {
-    if (
-      this.props.location.pathname !== prevProps.location.pathname ||
-      window.performance.navigation.type === 1
-    ) {
+    if (this.props.location.pathname !== prevProps.location.pathname) {
       window.scrollTo(0, 0)
     }
   }
