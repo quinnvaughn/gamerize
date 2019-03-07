61f5abb4a728c28bd76e9dec078552eccbd6c85d
Quinn Vaughn <quinn@gamerize.io>
2019-03-07 15:49:23 -0600

    Improved search bar.

--- client/src/Components/SearchBar.jsx
+++ client/src/Components/SearchBar.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
+import { FaSearch } from 'react-icons/fa'
 
 const Input = styled.input`
-  width: 30rem;
-  padding: 0.5rem 1.5rem;
-  box-sizing: border-box;
-  border-radius: 8px;
-  border: 1px solid black;
+  border: none;
+  outline: none;
+  width: 100%;
+  font-size: 1.8rem;
+  font-weight: 800;
+  margin-right: 0.4rem;
   :hover {
     cursor: text;
   }
@@ -15,14 +17,38 @@ const Input = styled.input`
   }
 `
 
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  height: 4.8rem;
+  padding: 1rem 0;
+  box-sizing: border-box;
+  border-radius: 4px;
+  border: 1px solid black;
+  width: 46rem;
+  border: 1px solid #ebebeb;
+  transition: box-shadow 200ms ease-in;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+`
+
+const StyledSearch = styled(FaSearch)`
+  margin-left: 1.2rem;
+  height: 1.8rem;
+  width: 1.8rem;
+  margin-right: 2rem;
+`
+
 export default function SearchBar(props) {
   const [search, setSearch] = useState('')
   return (
-    <Input
-      type="text"
-      placeholder="Search"
-      value={search}
-      onChange={event => setSearch(event.target.value)}
-    />
+    <Container>
+      <StyledSearch />
+      <Input
+        type="text"
+        placeholder="Search"
+        value={search}
+        onChange={event => setSearch(event.target.value)}
+      />
+    </Container>
   )
 }
