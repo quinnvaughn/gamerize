77b1b7aa862451520a76a4237065c14ec817d973
Quinn Vaughn <quinn@gamerize.io>
2019-03-21 15:20:53 -0500

    Edited Calendar.

--- src/resolvers/Query/gamingsession.js
+++ src/resolvers/Query/gamingsession.js
@@ -14,6 +14,18 @@ const gamingsession = {
       },
     })
   },
+  async thatDaySessions(parent, { day }, ctx) {
+    const userId = getUserId(ctx)
+    return await ctx.prisma.individualGamingSessions({
+      where: {
+        AND: [
+          { gamers_some: { id: userId } },
+          { startTime_gte: dateFns.startOfDay(new Date(day)) },
+          { startTime_lte: dateFns.endOfDay(new Date(day)) },
+        ],
+      },
+    })
+  },
 }
 
 module.exports = { gamingsession }
