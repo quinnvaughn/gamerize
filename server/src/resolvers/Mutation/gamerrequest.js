cfeadee8cb26f32f139b497e087f65c607d31a7a
Quinn Vaughn <quinn@gamerize.io>
2019-04-06 20:58:26 -0500

    Deleted gamer mutation file.

--- server/src/resolvers/Mutation/gamerrequest.js
+++ server/src/resolvers/Mutation/gamerrequest.js
@@ -1,40 +1,95 @@
 const { getUserId, AuthError } = require('../../utils')
+
 const gamerrequest = {
-  async acceptGamerRequest(parent, { input }, ctx) {
-    const userId = getUserId(ctx)
-    const user = await ctx.prisma.user({ id: userId })
-    if (!user.isAdmin) {
-      throw AuthError()
+  async createGamerRequest(parent, { input }, context) {
+    const userId = getUserId(context)
+    const alreadyRequested = await context.prisma.gamerRequests({
+      where: { user: { id: userId } },
+    })
+    if (alreadyRequested.length === 0) {
+      const request = await context.prisma.createGamerRequest({
+        addToOccupations: input.addToOccupations,
+        occupations: { set: input.occupations },
+        socialMedia: { create: input.socialMedia },
+        user: {
+          connect: {
+            id: userId,
+          },
+        },
+      })
+      return request
+        ? { request, created: true }
+        : { msg: 'Gamer request failed. Try again.', created: false }
     }
-    const newGamer = await ctx.prisma
-      .gamerRequest({ id: input.gamerRequestId })
+    return { msg: 'You already have a pending gamer request', created: false }
+  },
+  async acceptGamerRequest(parent, { input }, ctx) {
+    const adminId = getUserId(ctx)
+    const admin = await ctx.prisma.user({ id: adminId })
+    if (!admin.roles.includes('ADMIN')) {
+      throw new AuthError('You are not an admin')
+    }
+    const request = await ctx.prisma.gamerRequest({
+      id: input.gamerRequestId,
+    })
+    const user = await ctx.prisma
+      .gamerRequest({
+        id: input.gamerRequestId,
+      })
       .user()
     const updatedUser = await ctx.prisma.updateUser({
-      where: { id: newGamer.id },
+      where: { id: user.id },
       data: {
-        isGamer: true,
-      },
-    })
-    const notification = await ctx.prisma.createNotification({
-      type: 'ACCEPTED_GAMER_REQUEST',
-      text: `Congratulations ${
-        newGamer.name.split(' ')[0]
-      }! Your gamer request been accepted. Please fill out additional information so you can start hosting sessions.`,
-      for: {
-        connect: {
-          id: newGamer.id,
+        roles: {
+          set: ['USER', 'GAMER'],
         },
+        occupations: { set: request.occupations },
       },
     })
+    if (updatedUser) {
+      const deletedRequest = await ctx.prisma.deleteGamerRequest({
+        id: input.gamerRequestId,
+      })
+      const notification = await ctx.prisma.createNotification({
+        type: 'ACCEPTED_GAMER_REQUEST',
+        text: `Congratulations ${
+          user.name.split(' ')[0]
+        }! Your gamer request has been accepted. Please fill out additional information so you can start hosting sessions`,
+        for: {
+          connect: {
+            id: user.id,
+          },
+        },
+      })
+      return deletedRequest && notification
+        ? { accepted: true }
+        : { accepted: false }
+    }
+    return { accepted: false }
+  },
+  async declineGamerRequest(parent, { input }, ctx) {
+    const adminId = getUserId(ctx)
+    const admin = await ctx.prisma.user({ id: adminId })
+    if (!admin.roles.includes('ADMIN')) {
+      throw AuthError()
+    }
     const deletedRequest = await ctx.prisma.deleteGamerRequest({
       id: input.gamerRequestId,
     })
-    return updatedUser && notification && deletedRequest
+    const notification = await ctx.prisma.createNotification({
+      type: 'DECLINED_GAMER_REQUEST',
+      text: `Unfortunately your gamer request was not accepted. You can keep applying as you get more popular,`,
+      for: {
+        connect: {
+          id: user.id,
+        },
+      },
+    })
+
+    return deletedRequest && notification
       ? { accepted: true }
       : { accepted: false }
   },
 }
 
-module.exports = {
-  gamerrequest,
-}
+module.exports = { gamerrequest }
