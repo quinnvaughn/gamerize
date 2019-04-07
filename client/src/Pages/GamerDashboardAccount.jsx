6c17bc78da556482213ab21975e90628b827a130
Quinn Vaughn <quinn@gamerize.io>
2019-04-07 13:36:23 -0500

    Edited notifications.

---
+++ client/src/Pages/GamerDashboardAccount.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function GamerDashboardAccount(props) {
+  return (
+    <span>
+      <div>Hello</div>
+    </span>
+  )
+}
