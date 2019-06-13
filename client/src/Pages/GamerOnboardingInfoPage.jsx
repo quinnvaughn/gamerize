f19dd1e5586f981aa466ddb053ba933c195fe5a5
Quinn Vaughn <quinn@gamerize.io>
2019-06-13 18:00:00 -0500

    Removed weird text.

--- client/src/Pages/GamerOnboardingInfoPage.jsx
+++ client/src/Pages/GamerOnboardingInfoPage.jsx
@@ -216,9 +216,8 @@ function GamerOnboardingInfoPage(props) {
           </Subtitle>
           <Title>Most important</Title>
           <Subtitle>
-            Don't be an asshole  to the
-            gamers, or standoffish, or quiet. We want this to be a fun
-            experience for everyone.
+            Don't be an asshole to the gamers, or standoffish, or quiet. We want
+            this to be a fun experience for everyone.
           </Subtitle>
           <Stripe
             disabled={!disabled}
