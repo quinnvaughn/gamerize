92a52da92febdbc8ae38dc516a4f0da7fa0eca31
Quinn Vaughn <quinn@gamerize.io>
2019-06-27 12:26:55 -0500

    Added Lazy loading to gamer dashboard.

--- client/src/Pages/GamerDashboard.jsx
+++ client/src/Pages/GamerDashboard.jsx
@@ -1,17 +1,29 @@
-import React, { Fragment } from 'react'
+import React, { Suspense } from 'react'
 import gql from 'graphql-tag'
 import { useQuery } from 'react-apollo-hooks'
 import { Redirect } from 'react-router-dom'
 
-import GamerDashboardCalendar from './GamerDashboardCalendar'
-import GamerDashboardActiveSessions from './GamerDashboardActiveSessions'
-import GamerDashboardRetiredSessions from './GamerDashboardRetiredSessions'
-import GamerDashboardAccountEdit from './GamerDashboardAccountEdit'
-import GamerDashboardAccountPhotos from './GamerDashboardAccountPhotos'
 import GamerDashboardHome from './GamerDashboardHome'
 import GamerRoute from '../Components/GamerRoute'
 import useTitle from '../Hooks/useTitle'
 import ErrorPage from './ErrorPage'
+import Loading from '../Components/Loading'
+
+const GamerDashboardAccountPhotos = React.lazy(() =>
+  import('./GamerDashboardAccountPhotos')
+)
+const GamerDashboardAccountEdit = React.lazy(() =>
+  import('./GamerDashboardAccountEdit')
+)
+const GamerDashboardActiveSessions = React.lazy(() =>
+  import('./GamerDashboardActiveSessions')
+)
+const GamerDashboardRetiredSessions = React.lazy(() =>
+  import('./GamerDashboardRetiredSessions')
+)
+const GamerDashboardCalendar = React.lazy(() =>
+  import('./GamerDashboardCalendar')
+)
 
 const routes = [
   {
@@ -57,7 +69,7 @@ export default function GamerDashboard(props) {
   ) : error ? (
     <ErrorPage errors={error} />
   ) : (
-    <Fragment>
+    <Suspense fallback={<Loading gamer />}>
       {routes.map(route => (
         <GamerRoute
           path={route.path}
@@ -66,6 +78,6 @@ export default function GamerDashboard(props) {
           component={route.component}
         />
       ))}
-    </Fragment>
+    </Suspense>
   )
 }
