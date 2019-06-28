8b7c3224c1dc0d2b168b816f3358293be8368c1d
Quinn Vaughn <quinn@gamerize.io>
2019-06-28 11:39:36 -0500

    Changed language of button from cancel session to cancel booking.

--- client/src/Components/MyTimeSlot.jsx
+++ client/src/Components/MyTimeSlot.jsx
@@ -250,7 +250,7 @@ export default function MyTimeSlot({
                 }
               }}
             >
-              Cancel session
+              Cancel booking
             </CancelBooking>
           </CancelContainer>
         )}
